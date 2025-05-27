2fb0417c9711892f18db627810a2a485fbbc5193
pnumin <pnumin@pusan.ac.kr>
2025-05-27 12:15:41 +0900

    하트게임

--- 10/10.js
+++ 10/10.js
@@ -1,5 +1,10 @@
-
-
+const init = (cols) =>{
+      msg.innerHTML = "" ;
+      msg.innerHTML = "" ;
+      for (let col of cols) {
+        col.innerHTML = "" ;
+      }
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const cols = document.querySelectorAll(".col") ;
@@ -7,6 +12,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const msg = document.querySelector("#msg") ;
   let arr = [0,0,0,0,0,0,0,0,1] ;
   let flag = false ; 
+  let cnt = 0;
 
   for(let [idx, col] of cols.entries()) {
     // col.innerHTML = idx + 1 ;
@@ -15,15 +21,40 @@ document.addEventListener("DOMContentLoaded", ()=>{
         msg.innerHTML = "폭탄을 섞어주세요" ;
         return ;
       }
+      if (msg.innerHTML == "실패!")  return ;
+      
+      if ( arr[idx] == 0) {
+        //하트
+        col.innerHTML = '<img src="../img/hart.png">' ;
+        cnt++ ;
+
+        console.log(cnt, arr.indexOf(1))
+        if ( cnt == 8 ) {
+          cols[arr.indexOf(1)].innerHTML = '<img src="../img/hart.png">' ;
+          msg.innerHTML = "성공!" ;
+          flag = false ;
+          bt.innerHTML = "폭탄섞기" ;
+        }
+      }
+      else  {
+        //폭탄
+        col.innerHTML = '<img src="../img/boom.png">' ;
+        msg.innerHTML = "실패!" ;
+        flag = false ;
+        bt.innerHTML = "폭탄섞기" ;
+      }
     }) ;
   }
 
   bt.addEventListener("click", ()=>{
     if ( !flag ) {
       arr.sort(() => Math.random() - 0.5);
-      console.log(arr);
-      flag = true ;
       bt.innerHTML = "게임중 ...." ;
+      flag = true ;
+      
+      cnt = 0 ;
+      console.log(arr);
+      init(cols) ;
     }
   })
 });
