ddc63ec7dc23a920740c36b41730a9f150579e08
pnumin <pnumin@pusan.ac.kr>
2025-05-29 11:51:19 +0900

    일일박스오피스

--- 11/11.js
+++ 11/11.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  const ul = document.querySelector("main > ul") ;
-  const url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=2a350cfbca6c428eb04c71e21cc681e7&targetDt=20240528";
+const getMvList = (dt, ul) => {
+  console.log("dt=", dt) 
+  const url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=2a350cfbca6c428eb04c71e21cc681e7&targetDt=" + dt;
   
   // console.log(url)
   fetch(url) 
   .then(resp => resp.json())
   .then(data => {
     const dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList ;
+    console.log(dailyBoxOfficeList)
     const mvList = dailyBoxOfficeList.map((item) => 
            `<li>
                 <span class="spRank">${item.rank}</span> 
@@ -14,11 +15,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
             </li>` 
                                   ) ;
     let tags = mvList.join('') ;
-    console.log(tags)  
+     
     ul.innerHTML = tags ;                              
      
   })
   .catch(err => console.log(err)) 
   ;
+}
 
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const ul = document.querySelector("main > ul") ;
+  const dtIn = document.querySelector("#dt") ;
+
+  dtIn.addEventListener("change" , () => {
+    getMvList(dtIn.value.replaceAll('-',''), ul) ;
+  });
+  
 });
