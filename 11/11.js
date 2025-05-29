e93f9067859195e332d9e1f19a6db33bffcb148f
pnumin <pnumin@pusan.ac.kr>
2025-05-29 13:02:17 +0900

    박스오피스스

--- 11/11.js
+++ 11/11.js
@@ -1,3 +1,20 @@
+const yesterday = () => {
+  let yday = new Date() ;
+  yday.setDate(yday.getDate() - 1) ; //어제 날짜
+
+  return yday.toISOString().slice(0, 10);
+
+  // let y = yday.getFullYear() ; //연도4자리
+  // let m = yday.getMonth() + 1 ; //월 
+  // m = m < 10 ? '0' + m : m ;
+
+  // let d = yday.getDate() ; //일
+  // d = d < 10 ? '0' + d : d ;
+  
+  // return y+String(m).padStart(2, '0')+String(d).padStart(2,'0') ;
+}
+
+
 const getMvList = (dt, ul) => {
   console.log("dt=", dt) 
   const url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=2a350cfbca6c428eb04c71e21cc681e7&targetDt=" + dt;
@@ -12,6 +29,12 @@ const getMvList = (dt, ul) => {
            `<li>
                 <span class="spRank">${item.rank}</span> 
                 <span class="spMv">${item.movieNm}</span>
+                ${parseInt(item.rankInten) > 0 
+                  ? '<span class="spR"><i class="fa-solid fa-arrow-up"></i>' + Math.abs(item.rankInten) + "</span>"
+                  : parseInt(item.rankInten) < 0 
+                    ?'<span class="spB"><i class="fa-solid fa-arrow-down"></i>' + Math.abs(item.rankInten) + "</span>"
+                    :'<i class="fa-solid fa-minus sp"></i>'
+                }
             </li>` 
                                   ) ;
     let tags = mvList.join('') ;
@@ -23,10 +46,14 @@ const getMvList = (dt, ul) => {
   ;
 }
 
-
 document.addEventListener("DOMContentLoaded", ()=>{
   const ul = document.querySelector("main > ul") ;
   const dtIn = document.querySelector("#dt") ;
+  dtIn.setAttribute("max", yesterday()) ;
+
+  dtIn.value = yesterday() ; 
+  getMvList(dtIn.value.replaceAll('-',''), ul) ;
+  console.log(yesterday())
 
   dtIn.addEventListener("change" , () => {
     getMvList(dtIn.value.replaceAll('-',''), ul) ;
